a407d86c8b691399bd5f1ecacc540af6f0709091
erguotou <erguotou525@gmail.com>
2023-03-13 11:18:04 +0800

    feat: remove nonessential

--- custom-ts/auth/postAuthentication.ts
+++
@@ -1,6 +0,0 @@
-
-import { AuthenticationHookRequest } from 'fireboom-wundersdk/server'
-
-export default async function postAuthentication(hook: AuthenticationHookRequest) : Promise<void>{
-	// TODO: 在此处添加代码
-}
